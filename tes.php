fb14d1a01860719039b2ff4237d42fa2248e9f6e
maksudi2804 <maksudiindrarukmana@gmail.com>
2016-11-08 14:56:54 +0700

    Edit tes.php

--- tes.php
+++
@@ -1,7 +0,0 @@
-        <?php
-        $serverName = "CODAXFAMILY\SQL2014"; //nama server\nama instant, nomor port
-        $connectionInfo = array( "UID"=>"sa", "PWD"=>"1234", "Database"=>"RSLANGSA");
-        $conn = sqlsrv_connect( $serverName, $connectionInfo);
-        if( $conn === false ){
-                echo "Koneksi gagal</br>";
-        die( print_r( sqlsrv_errors(), true));
